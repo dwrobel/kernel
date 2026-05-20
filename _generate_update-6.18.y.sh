a407b99077e176b96e6daa5a697baeeb00b4246c
Damian Wrobel <dwrobel@ertelnet.rybnik.pl>
2026-05-20 10:06:44 +0200

    Update helper scripts for v6.18.x

--- _generate_update-6.18.y.sh
+++ _generate_update-6.18.y.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Copyright 2020 - 2025, Damian Wrobel <dwrobel@ertelnet.rybnik.pl>
+# Copyright 2020 - 2026, Damian Wrobel <dwrobel@ertelnet.rybnik.pl>
 # SPDX-License-Identifier: GPL-2.0
 
 set -euxo pipefail
@@ -11,12 +11,14 @@ set -euxo pipefail
 # stable	git://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git (fetch)
 # stable	git://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git (push)
 
-last_rebase=$(git log stable/linux-6.12.y --format="%H %s" -n50 | grep ' Linux 6.' | head -n 1)
+last_rebase=$(git log stable/linux-6.18.y --format="%H %s" -n50 | grep ' Linux 6.' | head -n 1)
+# For penultimate
+#last_rebase=$(git log stable/linux-6.18.y --format="%H %s" -n150 | grep ' Linux 6.' | head -n 2 | tail -n 1)
 
 sha_tag=$(echo $last_rebase | awk '{print $1}')
 sha_head=$(git rev-parse HEAD)
 
-path_name="bcm270x-linux-rpi-6.12.y-$(c=${sha_head}; echo ${c:0:9}).patch.xz"
+path_name="bcm270x-linux-rpi-6.18.y-$(c=${sha_head}; echo ${c:0:9}).patch.xz"
 
 echo git diff ${sha_tag}..${sha_head} to ${path_name}
 git diff ${sha_tag}..${sha_head} | xz -c9 > ${path_name}
@@ -24,7 +26,7 @@ git diff ${sha_tag}..${sha_head} | xz -c9 > ${path_name}
 micro_version=$(echo $last_rebase | awk '{print $3}')
 micro_version=$(echo $micro_version | awk -F '.' '{print $3}')
 
-kernel_dir=../fedberry-kernel-rpi-6.12.y
+kernel_dir=../fedberry-kernel-rpi-6.18.y
 
 cp -a $path_name ${kernel_dir}/
 (cd "${kernel_dir}" && ./update-spec.sh $micro_version $sha_head)
--- srpmbuild-all.sh
+++ srpmbuild-all.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 spectool -g kernel.spec && ./srpmbuild.sh && patch -p1 <kernel.diff && ./srpmbuild.sh && patch -p1 <kernel.diff -R && echo OK
+spectool -g kernel.spec && ./srpmbuild.sh && patch -p1 <kernel-rpi5.diff && ./srpmbuild.sh && patch -p1 <kernel-rpi5.diff -R && echo OK
 #patch -p1 <kernel-preempt.diff && spectool -g kernel.spec && ./srpmbuild.sh && patch -p1 <kernel.diff && ./srpmbuild.sh && patch -p1 <kernel.diff -R && patch -p1 <kernel-preempt.diff -R && echo OK
